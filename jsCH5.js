5bb57762ba702dd1788bc66756a3d0e65359c2b6
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-04-02 15:54:36 +0700

    first commit

---
+++ jsCH5.js
@@ -0,0 +1,11 @@
+Video.addEventListener("click", function video() {
+    alert("Ini Video James Arthur-Cars Outside");
+});
+
+Video.addEventListener("click", function video() {
+    alert("ini audio James Arthur-Cars Outside");
+});
+
+Video.addEventListener("click", function video() {
+    alert("ini gambar James Arthur");
+});
